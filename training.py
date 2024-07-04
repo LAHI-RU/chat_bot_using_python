7a736ee8caba694638211e42ee0bf755e750ff14
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-07-04 08:28:57 +0530

    update training.py

--- training.py
+++ training.py
@@ -2,3 +2,21 @@ import random
 import json
 import pickle
 import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classess = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+#for intent in intents['intents']
